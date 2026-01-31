2cc10ea1e8cf759664eafea1a4938fef5180df8e
abc15018045126 <abc15018045126@gmail.com>
2026-01-31 07:23:02 -0500

    Convert small utility classes to Kotlin - QuickQuoteHandler, NewlineHandler, Formatter, CodeBlock

--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/lang/format/Formatter.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/lang/format/Formatter.kt
@@ -1,82 +1,72 @@
+package io.github.abc15018045126.sora.lang.format
 
-package io.github.abc15018045126.sora.lang.format;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import io.github.abc15018045126.sora.text.Content;
-import io.github.abc15018045126.sora.text.TextRange;
+import io.github.abc15018045126.sora.text.Content
+import io.github.abc15018045126.sora.text.TextRange
 
 /**
  * Format content for editor
  */
-public interface Formatter {
-
+interface Formatter {
 
     /**
-     * Format the given content from {@code start} position to {@code end} position
-     * <p>
-     * Format the content directly, and call {@link FormatResultReceiver} to receive the formatted content from the editor when the formatting is complete
-     * *
+     * Format the given content from [cursorRange] position
+     *
+     * Format the content directly, and call [FormatResultReceiver] to receive the formatted content from the editor when the formatting is complete
      *
      * @param text        the content to format, but not the original Content in editor
      * @param cursorRange the positions of cursor. Start and end position may be the same.
      */
-    void format(@NonNull Content text, @NonNull TextRange cursorRange);
+    fun format(text: Content, cursorRange: TextRange)
 
     /**
-     * Format the given content from {@code start} position to {@code end} position
-     * <p>
-     * Format the content directly, and call {@link FormatResultReceiver} to receive the formatted content from the editor when the formatting is complete
+     * Format the given content from [rangeToFormat] position
+     *
+     * Format the content directly, and call [FormatResultReceiver] to receive the formatted content from the editor when the formatting is complete
      *
      * @param text          the content to format, but not the original Content in editor
      * @param rangeToFormat the range in text to be formatted
      * @param cursorRange   the positions of cursor. Start and end position may be the same.
      */
-    void formatRegion(@NonNull Content text, @NonNull TextRange rangeToFormat, @NonNull TextRange cursorRange);
+    fun formatRegion(text: Content, rangeToFormat: TextRange, cursorRange: TextRange)
 
     /**
      * Set the result receiver
      */
-    void setReceiver(@Nullable FormatResultReceiver receiver);
+    fun setReceiver(receiver: FormatResultReceiver?)
 
     /**
      * Whether the current formatter is running
      */
-    boolean isRunning();
+    fun isRunning(): Boolean
 
     /**
      * Destroy the formatter. Release any resources held.
      * Make sure that you will not call the receiver anymore.
      */
-    void destroy();
+    fun destroy()
 
     /**
      * Cancel last task if it is still running. Do not send success/failure to editor for last task.
      */
-    default void cancel() {
-
+    fun cancel() {
+        // Default implementation does nothing
     }
-
 
     interface FormatResultReceiver {
         /**
          * Called when the formatting is completed
          *
-         * @param applyContent the formatted <strong>full</strong> text
+         * @param applyContent the formatted **full** text
          * @param cursorRange  The range of cursor after formatting. You may pass null for unspecified.
          *                     Also, the start and end of the range may be the same position.
          */
-        void onFormatSucceed(@NonNull CharSequence applyContent, @Nullable TextRange cursorRange);
+        fun onFormatSucceed(applyContent: CharSequence, cursorRange: TextRange?)
 
         /**
          * Called when the formatting is failed
          *
          * @param throwable the throwable that caused formatting failed
          */
-        void onFormatFail(@Nullable Throwable throwable);
-
+        fun onFormatFail(throwable: Throwable?)
     }
-
 }
-
--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/lang/smartEnter/NewlineHandler.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/lang/smartEnter/NewlineHandler.kt
@@ -1,17 +1,13 @@
+package io.github.abc15018045126.sora.lang.smartEnter
 
-package io.github.abc15018045126.sora.lang.smartEnter;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import io.github.abc15018045126.sora.lang.styling.Styles;
-import io.github.abc15018045126.sora.text.CharPosition;
-import io.github.abc15018045126.sora.text.Content;
+import io.github.abc15018045126.sora.lang.styling.Styles
+import io.github.abc15018045126.sora.text.CharPosition
+import io.github.abc15018045126.sora.text.Content
 
 /**
  * Perform text processing when user enters '\n' and selection size is 0
  */
-public interface NewlineHandler {
+interface NewlineHandler {
 
     /**
      * Checks whether the given input matches the requirement to invoke this handler
@@ -21,7 +17,7 @@ public interface NewlineHandler {
      * @param style    Current code styles
      * @return Whether this handler should be called
      */
-    boolean matchesRequirement(@NonNull Content text, @NonNull CharPosition position, @Nullable Styles style);
+    fun matchesRequirement(text: Content, position: CharPosition, style: Styles?): Boolean
 
     /**
      * Handle newline and return processed content to insert
@@ -31,8 +27,6 @@ public interface NewlineHandler {
      * @param style    Current code styles
      * @return Actual content to insert
      */
-    @NonNull
-    NewlineHandleResult handleNewline(@NonNull Content text, @NonNull CharPosition position, @Nullable Styles style, int tabSize);
+    fun handleNewline(text: Content, position: CharPosition, style: Styles?, tabSize: Int): NewlineHandleResult
 
 }
-
