6484c9eed56370145c78f4297bf0eb6ab694bb94
abc15018045126 <abc15018045126@gmail.com>
2026-02-02 23:53:53 -0500

    1

--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/widget/Editor.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/widget/Editor.kt
@@ -1,29 +1,15 @@
 
-
 package io.github.abc15018045126.sora.widget
 
 import io.github.abc15018045126.sora.event.Event
 import io.github.abc15018045126.sora.event.EventManager.NoUnsubscribeReceiver
 import io.github.abc15018045126.sora.event.EventReceiver
-import io.github.abc15018045126.sora.event.SubscriptionReceipt
 import io.github.abc15018045126.sora.widget.component.EditorBuiltinComponent
 
+inline fun <reified T : Event> CodeEditor.subscribeAlways(receiver: NoUnsubscribeReceiver<T>) = subscribeAlways(T::class.java, receiver)
 
-inline fun <reified T : Event> CodeEditor.subscribeAlways(receiver: NoUnsubscribeReceiver<T>): SubscriptionReceipt<T> {
-    return subscribeAlways(T::class.java, receiver)
-}
+inline fun <reified T : Event> CodeEditor.subscribeEvent(receiver: EventReceiver<T>) = subscribeEvent(T::class.java, receiver)
 
+inline fun <reified T : EditorBuiltinComponent> CodeEditor.getComponent() = getComponent(T::class.java)
 
-inline fun <reified T : Event> CodeEditor.subscribeEvent(receiver: EventReceiver<T>): SubscriptionReceipt<T> {
-    return subscribeEvent(T::class.java, receiver)
-}
-
-
-inline fun <reified T : EditorBuiltinComponent> CodeEditor.getComponent(): T {
-    return getComponent(T::class.java)
-}
-
-
-inline fun <reified T : EditorBuiltinComponent> CodeEditor.replaceComponent(component: T) {
-    replaceComponent(T::class.java, component)
-}
+inline fun <reified T : EditorBuiltinComponent> CodeEditor.replaceComponent(component: T) = replaceComponent(T::class.java, component)
--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/widget/LanguageHelper.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/widget/LanguageHelper.kt
@@ -1,34 +1,16 @@
 package io.github.abc15018045126.sora.widget
 
 import io.github.abc15018045126.sora.lang.Language
-import io.github.abc15018045126.sora.lang.QuickQuoteHandler
 import io.github.abc15018045126.sora.text.ContentReference
 
-
 internal object LanguageHelper {
+    @JvmStatic fun getQuickQuoteHandler(language: Language) = try {
+        language.quickQuoteHandler
+    } catch (e: AbstractMethodError) { null }
 
-    @JvmStatic
-    fun getQuickQuoteHandler(language: Language): QuickQuoteHandler? {
-        return try {
-            language.quickQuoteHandler
-        } catch (e: AbstractMethodError) {
-            null
-        }
-    }
-
-    @JvmStatic
-    fun getIndentAdvance(
-        language: Language,
-        content: ContentReference,
-        line: Int,
-        column: Int,
-        spaceCountOnLine: Int,
-        tabCountOnLine: Int
-    ): Int {
-        return try {
-            language.getIndentAdvance(content, line, column, spaceCountOnLine, tabCountOnLine)
-        } catch (e: AbstractMethodError) {
-            language.getIndentAdvance(content, line, column)
-        }
+    @JvmStatic fun getIndentAdvance(language: Language, content: ContentReference, line: Int, column: Int, spaceCountOnLine: Int, tabCountOnLine: Int) = try {
+        language.getIndentAdvance(content, line, column, spaceCountOnLine, tabCountOnLine)
+    } catch (e: AbstractMethodError) {
+        language.getIndentAdvance(content, line, column)
     }
 }
