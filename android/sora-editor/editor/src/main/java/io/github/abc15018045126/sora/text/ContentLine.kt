e51c6a016ef43a716e34021951d67b7190ca5b46
abc15018045126 <abc15018045126@gmail.com>
2026-02-03 00:11:29 -0500

    1

--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/text/ContentLine.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/text/ContentLine.kt
@@ -7,33 +7,21 @@ import io.github.abc15018045126.sora.text.bidi.TextBidi
 import io.github.abc15018045126.sora.util.ShareableData
 import java.util.concurrent.atomic.AtomicInteger
 
-
 class ContentLine : CharSequence, GetChars, BidiRequirementChecker, ShareableData<ContentLine> {
-
     internal var value: CharArray
-    override var length: Int = 0
-        internal set
-
-    internal var rtlAffectingCount: Int = 0
-    internal var surrogateCount: Int = 0
+    override var length = 0; internal set
+    internal var rtlAffectingCount = 0
+    internal var surrogateCount = 0
     private var _lineSeparator: LineSeparator? = null
     var lineSeparator: LineSeparator?
         get() = _lineSeparator
-        set(value) {
-            _lineSeparator = value
-        }
+        set(v) { _lineSeparator = v }
 
-    val lineSeparatorSafe: LineSeparator
-        get() = _lineSeparator ?: LineSeparator.NONE
-
+    val lineSeparatorSafe get() = _lineSeparator ?: LineSeparator.NONE
     private var refCount: AtomicInteger? = null
 
-    constructor() : this(true)
-
-    constructor(text: CharSequence?) : this(true) {
-        insert(0, text)
-    }
-
+    constructor() : this(32)
+    constructor(text: CharSequence?) : this((text?.length ?: 0) + 16) { insert(0, text) }
     constructor(src: ContentLine) : this(src.length + 16) {
         length = src.length
         rtlAffectingCount = src.rtlAffectingCount
@@ -43,226 +31,131 @@ class ContentLine : CharSequence, GetChars, BidiRequirementChecker, ShareableDat
     }
 
     constructor(size: Int) {
-        length = 0
         value = CharArray(size)
     }
 
-    private constructor(initialize: Boolean) {
-        if (initialize) {
-            length = 0
-            value = CharArray(32)
-        } else {
-
-            value = CharArray(0)
-        }
+    private constructor(init: Boolean, size: Int = 0) {
+        value = if (init) CharArray(32) else CharArray(size)
     }
 
     private fun checkIndex(index: Int) {
-        if (index < 0 || index > length) {
-            throw StringIndexOutOfBoundsException("index = $index, length = $length")
+        if (index < 0 || index > length) throw StringIndexOutOfBoundsException("index=$index, length=$length")
+    }
+
+    private fun ensureCapacity(cap: Int) {
+        if (value.size < cap) {
+            val next = if (value.size * 2 < cap) cap + 2 else value.size * 2
+            value = CharArray(next).also { System.arraycopy(value, 0, it, 0, length) }
         }
     }
 
-    private fun ensureCapacity(capacity: Int) {
-        if (value.size < capacity) {
-            val newLength = if (value.size * 2 < capacity) capacity + 2 else value.size * 2
-            val newValue = CharArray(newLength)
-            System.arraycopy(value, 0, newValue, 0, length)
-            value = newValue
-        }
-    }
+    fun insert(dstOff: Int, s: CharSequence?) = insert(dstOff, s ?: "null", 0, (s ?: "null").length)
 
-    fun insert(dstOffset: Int, s: CharSequence?): ContentLine {
+    fun insert(dstOff: Int, s: CharSequence?, start: Int, end: Int): ContentLine {
         val str = s ?: "null"
-        return this.insert(dstOffset, str, 0, str.length)
-    }
-
-    fun insert(dstOffset: Int, s: CharSequence?, start: Int, end: Int): ContentLine {
-        val str = s ?: "null"
-        if (dstOffset < 0 || dstOffset > this.length) {
-            throw IndexOutOfBoundsException("dstOffset $dstOffset")
-        }
-        if (start < 0 || end < 0 || start > end || end > str.length) {
-            throw IndexOutOfBoundsException("start $start, end $end, s.length() ${str.length}")
-        }
+        if (dstOff < 0 || dstOff > length) throw IndexOutOfBoundsException("dstOff $dstOff")
+        if (start < 0 || end < 0 || start > end || end > str.length) throw IndexOutOfBoundsException("start $start, end $end")
         val len = end - start
         ensureCapacity(length + len)
-        System.arraycopy(value, dstOffset, value, dstOffset + len, length - dstOffset)
-        var currentOffset = dstOffset
+        System.arraycopy(value, dstOff, value, dstOff + len, length - dstOff)
         for (i in start until end) {
             val ch = str[i]
-            value[currentOffset++] = ch
-            if (TextBidi.couldAffectRtl(ch)) {
-                rtlAffectingCount++
-            }
-            if (Character.isSurrogate(ch)) {
-                surrogateCount++
-            }
+            value[dstOff + i - start] = ch
+            if (TextBidi.couldAffectRtl(ch)) rtlAffectingCount++
+            if (Character.isSurrogate(ch)) surrogateCount++
         }
         length += len
         return this
     }
 
-    fun insert(offset: Int, c: Char): ContentLine {
+    fun insert(off: Int, c: Char): ContentLine {
         ensureCapacity(length + 1)
-        if (offset < length) {
-            System.arraycopy(value, offset, value, offset + 1, length - offset)
-        }
-        if (TextBidi.couldAffectRtl(c)) {
-            rtlAffectingCount++
-        }
-        if (Character.isSurrogate(c)) {
-            surrogateCount++
-        }
-        value[offset] = c
-        length += 1
+        if (off < length) System.arraycopy(value, off, value, off + 1, length - off)
+        if (TextBidi.couldAffectRtl(c)) rtlAffectingCount++
+        if (Character.isSurrogate(c)) surrogateCount++
+        value[off] = c
+        length++
         return this
     }
 
     fun delete(start: Int, end: Int): ContentLine {
-        var e = end
-        if (start < 0) {
-            throw StringIndexOutOfBoundsException(start)
-        }
-        if (e > length) {
-            e = length
-        }
-        if (start > e) {
-            throw StringIndexOutOfBoundsException()
-        }
+        val e = if (end > length) length else end
+        if (start < 0 || start > e) throw StringIndexOutOfBoundsException("start $start, end $e")
         val len = e - start
         if (len > 0) {
             for (i in start until e) {
-                if (TextBidi.couldAffectRtl(value[i])) {
-                    rtlAffectingCount--
-                }
-                if (Character.isSurrogate(value[i])) {
-                    surrogateCount--
-                }
+                if (TextBidi.couldAffectRtl(value[i])) rtlAffectingCount--
+                if (Character.isSurrogate(value[i])) surrogateCount--
             }
-            System.arraycopy(value, start + len, value, start, length - e)
+            System.arraycopy(value, e, value, start, length - e)
             length -= len
         }
         return this
     }
 
-    override fun mayNeedBidi(): Boolean {
-        return rtlAffectingCount > 0
-    }
-
-    fun hasSurrogate(): Boolean {
-        return surrogateCount > 0
-    }
-
-    fun append(text: CharSequence): ContentLine {
-        return this.insert(length, text)
-    }
+    override fun mayNeedBidi() = rtlAffectingCount > 0
+    fun hasSurrogate() = surrogateCount > 0
+    fun append(text: CharSequence) = insert(length, text)
 
     @UnsupportedUserUsage
     override fun get(index: Int): Char {
         if (index >= length) {
             val sep = lineSeparatorSafe
-            return if (sep.length > 0) {
-                sep.content[index - length]
-            } else {
-                '\n'
-            }
+            return if (sep.length > 0) sep.content[index - length] else '\n'
         }
         return value[index]
     }
 
-    override fun subSequence(startIndex: Int, endIndex: Int): ContentLine {
-        checkIndex(startIndex)
-        checkIndex(endIndex)
-        if (endIndex < startIndex) {
-            throw StringIndexOutOfBoundsException("start is greater than end")
-        }
-        val subLen = endIndex - startIndex
-        val newValue = CharArray(subLen + 16)
-        System.arraycopy(value, startIndex, newValue, 0, subLen)
-        val res = ContentLine(false)
-        res.value = newValue
-        res.length = subLen
-
+    override fun subSequence(start: Int, end: Int): ContentLine {
+        checkIndex(start); checkIndex(end)
+        if (end < start) throw StringIndexOutOfBoundsException("end < start")
+        val len = end - start
+        val res = ContentLine(false, len + 16)
+        System.arraycopy(value, start, res.value, 0, len)
+        res.length = len
         if (rtlAffectingCount > 0) {
-            for (i in 0 until res.length) {
-                if (TextBidi.couldAffectRtl(newValue[i])) {
-                    res.rtlAffectingCount++
-                }
-                if (Character.isSurrogate(newValue[i])) {
-                    res.surrogateCount++
-                }
+            for (i in 0 until len) {
+                val c = res.value[i]
+                if (TextBidi.couldAffectRtl(c)) res.rtlAffectingCount++
+                if (Character.isSurrogate(c)) res.surrogateCount++
             }
         }
         return res
     }
 
-    fun appendTo(sb: StringBuilder) {
-        sb.append(value, 0, length)
-    }
-
-    override fun toString(): String {
-        return String(value, 0, length)
-    }
+    fun appendTo(sb: StringBuilder) = sb.append(value, 0, length)
+    override fun toString() = String(value, 0, length)
 
     fun toStringWithNewline(): String {
-        if (value.size == length) {
-            ensureCapacity(length + 1)
-        }
+        ensureCapacity(length + 1)
         value[length] = '\n'
         return String(value, 0, length + 1)
     }
 
-    val backingCharArray: CharArray
-        get() = value
+    val backingCharArray get() = value
 
     override fun getChars(srcBegin: Int, srcEnd: Int, dst: CharArray, dstBegin: Int) {
-        if (srcBegin < 0) {
-            throw StringIndexOutOfBoundsException(srcBegin)
-        }
-        if (srcEnd < 0 || srcEnd > length) {
-            throw StringIndexOutOfBoundsException(srcEnd)
-        }
-        if (srcBegin > srcEnd) {
-            throw StringIndexOutOfBoundsException("srcBegin > srcEnd")
-        }
+        if (srcBegin < 0 || srcEnd > length || srcBegin > srcEnd) throw StringIndexOutOfBoundsException()
         System.arraycopy(value, srcBegin, dst, dstBegin, srcEnd - srcBegin)
     }
 
-    fun copy(): ContentLine {
-        val clone = ContentLine(false)
-        clone.length = length
-        clone.value = CharArray(value.size)
-        System.arraycopy(value, 0, clone.value, 0, length)
-        clone.rtlAffectingCount = rtlAffectingCount
-        clone.surrogateCount = surrogateCount
-        clone._lineSeparator = _lineSeparator
-        return clone
+    fun copy() = ContentLine(false, value.size).apply {
+        length = this@ContentLine.length
+        System.arraycopy(this@ContentLine.value, 0, value, 0, length)
+        rtlAffectingCount = this@ContentLine.rtlAffectingCount
+        surrogateCount = this@ContentLine.surrogateCount
+        _lineSeparator = this@ContentLine._lineSeparator
     }
 
     override fun retain() {
-        if (refCount == null) {
-            refCount = AtomicInteger(2)
-            return
-        }
-        refCount!!.incrementAndGet()
+        if (refCount == null) refCount = AtomicInteger(2)
+        else refCount!!.incrementAndGet()
     }
 
     override fun release() {
-        if (refCount == null) {
-            return
-        }
-        val count = refCount!!.decrementAndGet()
-        if (count < 0) {
-            throw IllegalStateException("illegal operation. There is no active owner")
-        }
+        if (refCount != null && refCount!!.decrementAndGet() < 0) throw IllegalStateException("No active owner")
     }
 
-    override fun isMutable(): Boolean {
-        return refCount == null || refCount!!.get() == 1
-    }
-
-    override fun toMutable(): ContentLine {
-        return if (isMutable()) this else copy()
-    }
+    override fun isMutable() = refCount == null || refCount!!.get() == 1
+    override fun toMutable() = if (isMutable()) this else copy()
 }
