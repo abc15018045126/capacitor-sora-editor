dbf1407bb46e0b66f985fd2a1480542cce5b3733
abc15018045126 <abc15018045126@gmail.com>
2026-02-03 00:15:55 -0500

    1

--- android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/text/SpanRecycler.kt
+++ android/sora-editor/editor/src/main/java/io/github/abc15018045126/sora/text/SpanRecycler.kt
@@ -3,77 +3,32 @@ package io.github.abc15018045126.sora.text
 import android.util.Log
 import io.github.abc15018045126.sora.lang.styling.Span
 import java.util.concurrent.ArrayBlockingQueue
-import java.util.concurrent.BlockingQueue
 
 class SpanRecycler private constructor() {
-
-    private val taskQueue: BlockingQueue<MutableList<Span>> = ArrayBlockingQueue(8)
-    private var recycleThread: RecycleThread? = null
+    private val taskQueue = ArrayBlockingQueue<MutableList<Span>>(8)
+    private var recycleThread: Thread? = null
 
     fun recycle(spans: MutableList<Span>?) {
-        if (spans == null) {
-            return
-        }
-        if (recycleThread == null || !recycleThread!!.isAlive) {
-            recycleThread = RecycleThread()
-            recycleThread!!.start()
+        if (spans == null) return
+        if (recycleThread?.isAlive != true) {
+            recycleThread = Thread {
+                try {
+                    while (!Thread.interrupted()) {
+                        val list = taskQueue.take()
+                        var count = 0
+                        for (s in list) if (s.recycle()) count++ else break
+                        list.clear()
+                        Log.i(LOG_TAG, "Recycled $count spans")
+                    }
+                } catch (e: Exception) { Log.w(LOG_TAG, e) }
+            }.apply { isDaemon = true; name = "SpanRecycleDaemon"; start() }
         }
         taskQueue.offer(spans)
     }
 
-    private inner class RecycleThread internal constructor() : Thread() {
-
-        init {
-            isDaemon = true
-            name = "SpanRecycleDaemon"
-        }
-
-        override fun run() {
-            try {
-                while (!isInterrupted) {
-                    try {
-                        val spans = taskQueue.take()
-                        var count = 0
-                        val size = spans.size
-                        for (i in 0 until size) {
-                            val span = spans[i]
-                            if (!span.recycle()) {
-
-
-
-
-
-
-
-                                break
-                            }
-                            count++
-                        }
-                        spans.clear()
-                        Log.i(LOG_TAG, "Called recycle() on $count spans")
-                    } catch (e: InterruptedException) {
-                        e.printStackTrace()
-                        break
-                    }
-                }
-            } catch (e: Exception) {
-                Log.w(LOG_TAG, e)
-            }
-            Log.i(LOG_TAG, "Recycler exited")
-        }
-    }
-
     companion object {
         private const val LOG_TAG = "SpanRecycler"
-        private var INSTANCE: SpanRecycler? = null
-
-        @JvmStatic
-        @Synchronized
-        fun getInstance(): SpanRecycler {
-            if (INSTANCE == null) {
-                INSTANCE = SpanRecycler()
-            }
-            return INSTANCE!!
-        }
+        private val INSTANCE by lazy { SpanRecycler() }
+        @JvmStatic fun getInstance() = INSTANCE
     }
 }
